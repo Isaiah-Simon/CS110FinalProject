767fd64b44f78ec7414dd6f3ad4ea61af4142247
Isaiah-Simon <zays1993@gmail.com>
2014-04-26 00:02:25 -0400

    Created pile class

---
+++ Pile.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+// Isaiah Simon
+// CS 110
+// Pile
+
+/**
+   The Pile class holds the information about a pile object.
+*/
+
+public class Pile
+{   
+   //Declare Variables
+   ArrayList<Card> pile = new ArrayList<Card>(); 
+
+   /**
+   This constructor creates a new pile of cards
+   */
+   Pile()
+   {
+   
+   }
+
+
+}
