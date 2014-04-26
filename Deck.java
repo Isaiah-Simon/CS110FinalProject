cc7864f54f3cd579fad92b6aab621805e66bc625
Isaiah-Simon <zays1993@gmail.com>
2014-04-25 23:18:29 -0400

    Added constructor
    Added default constructor that creates each card and adds it to the
    deck.

--- Deck.java
+++ Deck.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
+
 // Isaiah Simon
 // CS 110
 // Deck
@@ -8,5 +12,39 @@
 
 public class Deck
 { 
+   //Declare Constants
+   final static int SUIT_CARD_AMOUNT = 13; //The amount of cards in each suit
+   ArrayList<Card> deck = new ArrayList<Card>(); 
 
-}
+   /**
+   This constructor creates a deck of cards
+   */
+
+   public Deck()
+   {
+      newDeck();
+   }
+   
+   public void newDeck ()
+   {
+      for(int i=2;i<SUIT_CARD_AMOUNT;i++)
+      {
+         //Adding heart cards
+         Card c1 = new Card('h',i);
+         deck.add(c1);
+         
+         //Adding spade cards
+         Card c2 = new Card('s',i);
+         deck.add(c2);
+         
+         //Adding club cards
+         Card c3 = new Card('c',i);
+         deck.add(c3);
+         
+         //Adding diamond cards
+         Card c4 = new Card('d',i);
+         deck.add(c4);
+      }
+   }
+   
+}  
