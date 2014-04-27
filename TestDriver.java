d2e94db5f4fcb0756360af4500f2a984a75f6321
Isaiah-Simon <zays1993@gmail.com>
2014-04-27 18:20:18 -0400

    Adding files so I can merge

---
+++ TestDriver.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+// Isaiah Simon
+// CS 110
+// Skeleton
+
+public class TestDriver
+{
+   public static void main(String [] args)
+   {
+     // declare variables
+     
+     // Create a scanner object 
+     
+//      Card c = new Card('h',10);
+//      System.out.println(c);
+//      System.out.println("images/" + c + ".jpg");
+//          
+//         Deck d = new Deck(); 
+//         Pile p1 = new Pile();
+//         Pile p2 = new Pile();
+//         
+//         System.out.println(p1);
+//         
+//         for(int i=0; i<52; i+=2)
+//         {
+//            Card tempCard1 = new Card(d.getCard(i));
+//            Card tempCard2 = new Card(d.getCard(i+1));
+//            
+//            p1.addCard(tempCard1);
+//            p2.addCard(tempCard2);
+//         }
+//         System.out.println(p1);
+//         System.out.println(p2);
+//         
+//         ArrayList<Card> cards1 = p1.warDraw(p1); 
+//         System.out.println(cards1);
+//         Card[] cards2 = new Array(p2.warDraw()); 
+
+         Card c1 = new Card('h',12);
+         System.out.println(c1.getCardImage());
+   }
+   
+}
