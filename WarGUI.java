b9205023a92c3e4bbb7ef0da0887fe692af02b5e
Isaiah-Simon <zays1993@gmail.com>
2014-04-27 04:42:49 -0400

    Adding intial file

---
+++ WarGUI.java
@@ -0,0 +1,22 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+// Isaiah Simon
+// CS 110
+// War GUI
+
+public class WarGUI
+{
+
+}
