5ed5fbeccb797880a00bde6272b240c9491e9a94
Isaiah-Simon <zays1993@gmail.com>
2014-04-27 00:47:25 -0400

    Added decks and piles.
    Also split deck into two piles

--- War.java
+++ War.java
@@ -9,9 +9,24 @@ public class War
    public static void main(String [] args)
    {
      // declare variables
+     Deck deck = new Deck();
+     Pile player1 = new Pile();
+     Pile player2 = new Pile();
      
-     // Create a scanner object 
+     //Splits deck into two piles
+     for(int i=0; i<52; i+=2)
+     {
+        Card tempCard1 = new Card(deck.getCard(i));
+        Card tempCard2 = new Card(deck.getCard(i+1));
+        
+        player1.addCard(tempCard1);
+        player2.addCard(tempCard2);
+     }
      
+     System.out.println("Let the game begin!");
+
+     
+     // Create a scanner object  
      Scanner in = new Scanner(System.in);
      
      
