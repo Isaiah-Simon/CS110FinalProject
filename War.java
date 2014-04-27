2d6232a9b90acc32dd682a09786b311ed1f80b99
Isaiah-Simon <zays1993@gmail.com>
2014-04-27 00:43:51 -0400

    Created initial war file

---
+++ War.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// Isaiah Simon
+// CS 110
+// War
+
+public class War
+{
+   public static void main(String [] args)
+   {
+     // declare variables
+     
+     // Create a scanner object 
+     
+     Scanner in = new Scanner(System.in);
+     
+     
+   }
+   
+}
